f9d09daaea351bd25691b6f9f0e99bacb7bf63a4
leszkokata <leszkokata@gmail.com>
2020-11-18 00:38:35 +0100

    admin page with mock data

--- vir4fe/src/app/admin/admin.component.ts
+++ vir4fe/src/app/admin/admin.component.ts
@@ -1,5 +1,29 @@
 import { Component, OnInit } from '@angular/core';
 
+export interface User {
+  username: string;
+  jpg: boolean;
+  png: boolean;
+  gif: boolean;
+}
+
+const DATA: User[] = [
+  { username: 'joska',
+  jpg: true,
+  png: true,
+  gif: false
+},
+{ username: 'viri',
+  jpg: true,
+  png: false,
+  gif: false
+},
+{ username: 'piri',
+  jpg: true,
+  png: true,
+  gif: true
+},
+]
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
@@ -7,6 +31,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
+  displayedColumns: string[] = ['username', 'jpg', 'png', 'gif'];
+  dataSource = DATA;
   constructor() { }
 
   ngOnInit(): void {
